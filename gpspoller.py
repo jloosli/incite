af6a456c76b73d4d5cec21da8b8ec02119b79ea4
Jared Loosli <jloosli@gmail.com>
2013-05-16 11:53:30 -0600

    gpspoller is working

--- gpspoller.py
+++ gpspoller.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
 import threading
 import time
 from gps import *
+
+gpsd = None # Set the global variable
 
 
 class GpsPoller(threading.Thread):
@@ -8,26 +13,53 @@ class GpsPoller(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
-        self.session = gps(mode=WATCH_ENABLE)
+        self.daemon = True
+        global gpsd
+        gpsd = gps()
+        gpsd.stream(WATCH_ENABLE | WATCH_NEWSTYLE)
         self.current_value = None
+        self.running = True
 
     def get_current_value(self):
         return self.current_value
 
     def run(self):
-        try:
-            while True:
-                self.current_value = self.session.next()
-        except StopIteration:
-            pass
+        global gpsd
+        while self.running:
+            self.current_value=gpsd.next()
 
 
 def main():
     gpsp = GpsPoller()
-    gpsp.run()
+    try:
+        gpsp.start()
+        while 1:
+            print "Getting current Value:"
+            print gpsp.get_current_value()
+        time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        print "\nKilling Thread..."
+        gpsp.running = False
+        gpsp.join()
+    print "Done.\nExiting."
+
+def main2():
+    session = gps()
+    session.stream(WATCH_ENABLE | WATCH_NEWSTYLE)
     while 1:
-        print gpsp.get_current_value()
-        time.sleep(1)
+        try:
+            report = session.next()
+            print "\n" , report , "\n"
+            if report['class'] == 'TPV':
+                if hasattr(report, 'time'):
+                    print report.time
+        except KeyError:
+            pass
+        except KeyboardInterrupt:
+            quit()
+        except StopIteration:
+            session = None
+            print "GPSD has terminated"
 
 if __name__ == '__main__':
     main()
